4391131042d1e4b3cca7ed3a1a119200b5237b35
wenke <Z$^Pmv&X$PniZDks>
2023-06-20 14:11:01 +0800

    vue基础

---
+++ Vue3/01_webpack+vue基础/vue/vue_demo/src/10.v-text和v-html区别.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p v-text="str"></p>
+    <p v-html="str"></p>
+    <!-- 注意: v-text或v-html会覆盖插值表达式 -->
+    <!-- error => Child content is disallowed because it will be overwritten by the v-html directive -->
+    <!-- <p v-html="str">{{ 10 + 20 }}</p> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      str: "<span>我是一个span</span>"
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ Vue3/01_webpack+vue基础/vue/vue_demo/src/练习1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(val, index) in arr" :key="index">{{ val }}</li>
+    </ul>
+    <button @click="btn"> go</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [1, 2, 3]
+    }
+  },
+  methods: {
+    btn() {
+      // let newArr = this.arr
+      let first = this.arr.shift()
+      this.arr.push(first)
+      // this.arr = newArr
+    }
+  }
+}
+</script>
+
+<style></style>
